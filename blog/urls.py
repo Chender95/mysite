800d4875e1f0cfb2e654b9b8d9a95d023184de4a
Yunus <zubenko20001@gmail.com>
2023-06-09 14:27:38 +0300

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('shava/', shava, name='shava' )
+    path('shava/', shava, name='shava'),
+    re_path('r^archive/(?P<year>[0-9]{4})/', archive)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponseNotFound
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1><i>страница не нашлась!</i></h1>')
 
 def post_list(request):
-    return HttpResponse('<h1>Главная страница. Пишу через HttpResponse. Крутая штука но на ней далеко не уйдешь!')
+    return render(request, 'blog/post_list.html', {})
 
 
 def shava(request):
     return render(request, 'blog/shava.html', {})
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('/', permanent=True)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
